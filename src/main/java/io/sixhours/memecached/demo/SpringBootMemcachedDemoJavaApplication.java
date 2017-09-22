8e15e8292780c72f5ff84beb5b51dadaa11aeda4
Sasa Bolic <sasa.bolic@sixhours.io>
2017-09-22 12:01:10 +0200

    Initial commit

---
+++ src/main/java/io/sixhours/memecached/demo/SpringBootMemcachedDemoJavaApplication.java
@@ -0,0 +1,12 @@
+package io.sixhours.memecached.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMemcachedDemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMemcachedDemoJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/io/sixhours/memecached/demo/SpringBootMemcachedDemoJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package io.sixhours.memecached.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootMemcachedDemoJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
