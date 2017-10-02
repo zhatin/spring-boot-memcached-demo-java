56eb2c91a187e09b01abbf7b1a80cf92a8f350d1
Sasa Bolic <sasa.bolic@sixhours.io>
2017-10-02 11:22:05 +0200

    Application class reanmed and caching enabled

--- src/main/java/io/sixhours/memecached/demo/Application.java
+++ src/main/java/io/sixhours/memecached/demo/Application.java
@@ -2,11 +2,13 @@ package io.sixhours.memecached.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class SpringBootMemcachedDemoJavaApplication {
+@EnableCaching
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMemcachedDemoJavaApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
